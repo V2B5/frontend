8f8f7f0aa6130077050f83b179ace5fcc0416895
V2B5 <117782333+V2B5@users.noreply.github.com>
2024-07-16 11:35:26 +0100

    Update MostComments.js

--- src/views/home/MostComments.js
+++ src/views/home/MostComments.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Card, CardContent, Typography } from "@mui/material";
-import { styled } from "@mui/system";
+//import { styled } from '@mui/system';
+import styled from '@emotion/styled';
 import { Link } from "react-router-dom";
 import api from '../api/api';
 
